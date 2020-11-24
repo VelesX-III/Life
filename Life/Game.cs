19023c4d05ead7337cb5c9bf148815010617fefa
Sephiran <robertothomas427@gmail.com>
2020-11-23 19:05:24 -0800

    Add project files.

---
+++ Life/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    public class Game
+    {
+        public int Period { get; private set; }
+        public int Health { get; private set; }
+        private readonly float k;
+        private readonly float g; //Gamma
+        private readonly int v;
+        public bool[,] Field { get; private set; }
+
+        public Game(int m, int n, int t, int v, float g, float k)
+        {
+            Field = new bool[m, n];
+            Random random = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Field[i, j] = random.Next(0, 2) == 1 ? true : false;
+                }
+            }
+            this.k = k;
+            this.g = g;
+            this.v = v;
+        }
+    }
+}
---
+++ Life/Individual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    abstract class Individual
+    {
+        public IList<int> Chromosomes { get; protected set; }
+        public abstract float FitnessScore();
+        public abstract Individual Cross(Individual otherParent);
+        public abstract Individual Mutate();
+    }
+}
