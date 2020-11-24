04d7b38b726bb33e7fb407a262f51d8392920de5
Sephiran <robertothomas427@gmail.com>
2020-11-23 19:23:25 -0800

    Added comments.

--- Life/Game.cs
+++ Life/Game.cs
@@ -8,15 +8,31 @@ namespace Life
 {
     public class Game
     {
+        /// <summary>
+        /// The current period of the game.
+        /// </summary>
         public int Period { get; private set; }
+        /// <summary>
+        /// The player's current amount of health.
+        /// </summary>
         public int Health { get; private set; }
+        /// <summary>
+        /// The constant k, which affects health regeneration.
+        /// </summary>
         private readonly float k;
-        private readonly float g; //Gamma
+        /// <summary>
+        /// The constant gamma, which affects harvesting.
+        /// </summary>
+        private readonly float g;
+        /// <summary>
+        /// The constant v, which is the value of a successful harvesting attempt.
+        /// </summary>
         private readonly int v;
         public bool[,] Field { get; private set; }
-
-        public Game(int m, int n, int t, int v, float g, float k)
+        public Game(int periods, int defaultHealth,int m, int n, int t, int v, float g, float k)
         {
+            Health = defaultHealth;
+            Period = periods;
             Field = new bool[m, n];
             Random random = new Random();
             for (int i = 0; i < m; i++)
--- Life/Individual.cs
+++ Life/Individual.cs
@@ -8,9 +8,24 @@ namespace Life
 {
     abstract class Individual
     {
-        public IList<int> Chromosomes { get; protected set; }
+        /// <summary>
+        /// Presumably the choices made by the player?
+        /// </summary>
+        public IEnumerable<int> Chromosomes { get; protected set; }
+        /// <summary>
+        /// The fitness score function of the GA.
+        /// </summary>
+        /// <returns>A real number measuring the fitness of the GA. Presumably the same as total life enjoyment.</returns>
         public abstract float FitnessScore();
+        /// <summary>
+        /// Gets the child of the current individual and the individual specified by the parameter.
+        /// </summary>
+        /// <param name="otherParent">The individual to cross with.</param>
+        /// <returns>The child individual.</returns>
         public abstract Individual Cross(Individual otherParent);
-        public abstract Individual Mutate();
+        /// <summary>
+        /// Mutates the current individual, shuffling its chromosomes.
+        /// </summary>
+        public abstract void Mutate();
     }
 }
