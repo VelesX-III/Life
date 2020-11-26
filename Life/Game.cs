5b8aa50231c1971a549ae90ffa8eda252c2f3c1f
Sephiran <robertothomas427@gmail.com>
2020-11-25 16:52:32 -0800

    Worked more the game part of the problem.

--- Life/Game.cs
+++ Life/Game.cs
@@ -6,45 +6,90 @@ using System.Threading.Tasks;
 
 namespace Life
 {
+    /// <summary>
+    /// Represets a single game.
+    /// </summary>
     public class Game
     {
         /// <summary>
         /// The current period of the game.
         /// </summary>
-        public int Period { get; private set; }
+        public uint Period { get; private set; }
         /// <summary>
         /// The player's current amount of health.
         /// </summary>
-        public int Health { get; private set; }
+        public double Health { get; private set; }
         /// <summary>
-        /// The constant k, which affects health regeneration.
+        /// The amount of the player's life enjoyment. You want to optimize this.
         /// </summary>
-        private readonly float k;
+        public double LifeEnjoyment { get; private set; }
         /// <summary>
-        /// The constant gamma, which affects harvesting.
+        /// The constant <i>k</i>, which affects health regeneration.
         /// </summary>
-        private readonly float g;
+        private readonly double k;
         /// <summary>
-        /// The constant v, which is the value of a successful harvesting attempt.
+        /// The constant <i>γ</i>, which affects harvesting.
+        /// </summary>
+        private readonly double g;
+        /// <summary>
+        /// The constant <i>v</i>, which is the value of a successful harvesting attempt.
         /// </summary>
         private readonly int v;
+        /// <summary>
+        /// The constant <i>c</i>, which affects life enjoyment.
+        /// </summary>
+        private readonly double c;
+        /// <summary>
+        /// The constant <i>α</i>, which affects life enjoyment.
+        /// </summary>
+        private readonly double a;
+        /// <summary>
+        /// The harvesting field, represented as a multidimensional array.
+        /// You get <i>v</i> points of life enjoyment for a successful harvest,
+        /// at one of the <i>t</i> points scattered across the field.
+        /// </summary>
         public bool[,] Field { get; private set; }
-        public Game(int periods, int defaultHealth,int m, int n, int t, int v, float g, float k)
+        /// <summary>
+        /// Constructs a new game with the default parameters. You can override these.
+        /// </summary>
+        /// <param name="periods">Number of periods to begin the game with.</param>
+        /// <param name="defaultHealth">Starting amount of health to begin the game with.</param>
+        /// <param name="m">Number of rows of the field.</param>
+        /// <param name="n">Number of columns in the field.</param>
+        /// <param name="t">Number of successful harvest points dispersed across the field.</param>
+        /// <param name="v">Value of life enjoyment for a successful harvest.</param>
+        /// <param name="g">Affects harvesting.</param>
+        /// <param name="k">Affects health regen.</param>
+        /// <param name="c">Affects life enjoyment.</param>
+        /// <param name="a">Affects life enjoyment.</param>
+        public Game(uint periods = 10, float defaultHealth = 70, int m = 10, int n = 10, int t = 100, int v = 1, double g = 1, double k = 0.01021, double c = 464.53, double a = 32)
         {
             Health = defaultHealth;
             Period = periods;
+            this.k = k;
+            this.g = g;
+            this.v = v;
+            this.c = c;
+            this.a = a;
             Field = new bool[m, n];
             Random random = new Random();
             for (int i = 0; i < m; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    Field[i, j] = random.Next(0, 2) == 1 ? true : false;
+                    if (t > 0 && random.Next(0, 2) == 1)
+                    {
+                        Field[i, j] = true;
+                        t--;
+                    }
                 }
             }
-            this.k = k;
-            this.g = g;
-            this.v = v;
         }
+        /// <summary>
+        /// Regenerates health.
+        /// </summary>
+        /// <param name="I">The health investment.</param>
+        /// <param name="H">The health after harvesting.</param>
+        private void HealthRegen(double I, double H) => Health += 100 * (Math.Pow(Math.E, k * I) / (Math.Pow(Math.E, k * I) + ((100 - H) / H))) - H;
     }
 }
--- Life/Program.cs
+++ Life/Program.cs
@@ -10,7 +10,7 @@ namespace Life
     {
         static void Main(string[] args)
         {
-
+            Game g = new Game();
         }
     }
 }
